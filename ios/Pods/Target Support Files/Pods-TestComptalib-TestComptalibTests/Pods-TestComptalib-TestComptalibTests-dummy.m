cfffd04842dddd7f0032aa5330e51a9db5fc7e1f
clemlvt <clemlvt42@gmail.com>
2021-08-27 09:17:02 +0200

    First commit

---
+++ ios/Pods/Target Support Files/Pods-TestComptalib-TestComptalibTests/Pods-TestComptalib-TestComptalibTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestComptalib_TestComptalibTests : NSObject
+@end
+@implementation PodsDummy_Pods_TestComptalib_TestComptalibTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-TestComptalib/Pods-TestComptalib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestComptalib : NSObject
+@end
+@implementation PodsDummy_Pods_TestComptalib
+@end
